d8952ebaf45a6c3d55f2a746200779b38f91fce3
Daniil <111334044+wuzimoo@users.noreply.github.com>
2023-09-13 10:44:08 +0300

    debug 0.2

--- hpcctable/table/main.py
+++ hpcctable/table/main.py
@@ -13,7 +13,7 @@ class table:
     service = None
     httpAuth = None
     credentials = None
-    sheet_name = NoneП
+    sheet_name = None
 
     def __init__(self) -> None:
         CREDENTIALS_FILE = self.path  # Имя файла с закрытым ключом, вы должны подставить свое
